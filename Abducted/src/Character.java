8aa4247447e55fda65aa4b5e07610fa58c57a478
Bailey Peterson <Colegkid@gmail.com>
2022-04-10 13:23:17 -0500

    checks death status and exits if they are dead

--- Abducted/src/Character.java
+++ Abducted/src/Character.java
@@ -5,5 +5,13 @@ public class Character {
 	
 	public class UserCharacter{
 		String name = "";
+		boolean isdead = false;
+		//checks if user is dead
+		public void ChecksDeathStatus(boolean deathstatus) {
+			if (deathstatus = true)
+			{
+				System.exit(0);
+			}
+		}
 	}
 }
