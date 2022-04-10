d9bb6283c0d4395fc69f6b7858f6553eb88cf301
Bailey Peterson <Colegkid@gmail.com>
2022-04-10 17:56:12 -0500

    Adds Starting Room

---
+++ Abducted/src/Starting_Room.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Bailb
+ *
+ */
+public class Starting_Room implements Room {
+	public String RoomName = "Airlock";
+	
+	public void EnterRoom() {
+		System.out.println("You wake up and feel cold metal pressing against your cheek as you open your eyes you see a strange corpse lying next to you. ");
+		
+	}
+
+}
