1a67f2abb7e52da731eda1c1344bd75e1accc98a
Bailey Peterson <Colegkid@gmail.com>
2022-04-10 17:46:01 -0500

    commenting

--- Abducted/src/Room.java
+++ Abducted/src/Room.java
@@ -1,6 +1,12 @@
 
 public interface Room {
+	/**
+	 * Is the individual name for each room
+	 */
 	public String RoomName = "";
+	/**
+	 * displays unique message for each room
+	 */
 	private void EnterRoom() {};
 
 	
