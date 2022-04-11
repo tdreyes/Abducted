71be9dd4c4875ed7f089959684576f6ed87acd34
Bailey Peterson <Colegkid@gmail.com>
2022-04-10 19:13:34 -0500

    Added Final to RoomName

--- Abducted/src/Starting_Room.java
+++ Abducted/src/Starting_Room.java
@@ -7,7 +7,7 @@
  *
  */
 public class Starting_Room implements Room {
-	public String RoomName = "Airlock";
+	public final String RoomName = "Airlock";
 	
 	@Override public void EnterRoom() {
 		System.out.println("You start the game in a dark poorly lit room. The room is small, maybe 6 feet by 8 feet, the room contains 4 walls, two of which have windows, one window views the adjacent room, you turn around to look out the other window to be greeted with the endless void of space. Panicked on how to escape this place you look around  the entire room.");
