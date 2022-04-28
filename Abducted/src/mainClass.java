d35f7d1066ea690fb06c7f8f99ed92ac1f452028
Bailey Peterson <Colegkid@gmail.com>
2022-04-28 13:02:25 -0500

    Fixed Input problems partially

--- Abducted/src/mainClass.java
+++ Abducted/src/mainClass.java
@@ -6,24 +6,41 @@ public class mainClass {
 		//Teresa added the intro message
 				System.out.println("Welcome to Abducted");
 				System.out.println("Please type enter to start");
-				//Baileys Push
 				
 				/**
 				 * this is to check user input
 				 */
 				Scanner uAction = new Scanner(System.in);
-				
-				String uInput = uAction.nextLine();
-				
+				String uInput;
+				// Bailey Fixed input results
 				while(uAction.hasNext()) {
-					System.out.println(uAction.next());
-					if (uAction.toString() == "h") {
+					uInput = uAction.next();
+					uInput += uAction.nextLine();
+					System.out.println(uInput);
+					if (uInput == "h") {
+						// Bailey added all the current possible actions
+						System.out.println("Possible Actions Include:");
+						System.out.println("USE");
+						System.out.println("LOOK");
+						System.out.println("TAKE");
+						System.out.println("SMASH");
+						System.out.println("SHOOT");
+						System.out.println("RELOAD");
+						System.out.println("HIDE");
+						System.out.println("You May also Look in your inventory by typing either I or Inventory");
+						System.out.println("You may also type E or Exit At any time to exit the game");
+					}
+					// Bailey added a search inventory command
+					if (uInput == "I" || uInput == "Inventory") {
 						
 					}
+					// Bailey added a search inventory command
+					if (uInput == "E" || uInput == "Exit") {
+						uAction.close();
+						System.exit(0);
+					}
 				}
-		System.out.println("Welcome to Abducted");
-		System.out.println("Please type enter to start");
-		
+				
 	}
 
 }
